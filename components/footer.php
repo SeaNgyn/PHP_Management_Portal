b94ad0dfebe9a837aaab71e1a94effdb51e4903c
SeaNgyn <nguyentheanh2k3@gmail.com>
2025-04-28 23:04:08 +0700

    delete to create new prj

--- components/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="bg-light text-center text-lg-start">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);"> © 2025 Copyright Nguyen The Anh - PHP </div>
-</footer>
